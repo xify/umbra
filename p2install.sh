05863f1d9ccaacda00d2e9fd440918aa45791463
Seok-Ho Yang <xify@me.com>
2012-07-23 14:24:09 +0900

    Initial commits

---
+++ p2install.sh
@@ -0,0 +1,26 @@
+. ./env.rc
+
+INSTALL_NAME=juno
+PROFILE_NAME=juno
+PACKAGE_NAME=epp.package.reporting
+
+# <profile> <top-level iu>
+function install {
+    echo Installing base artifacts of Eclipse IDE
+java -jar ${PLATFORM_RUNTIME_HOME}/plugins/org.eclipse.equinox.launcher_*.jar \
+    -nosplash \
+    -application org.eclipse.equinox.p2.director \
+    -repository ${BASE_REPO},http://ftp.jaist.ac.jp/pub/eclipse/technology/epp/packages/juno/ \
+    -installIU $2 \
+    -destination ${INSTALL_HOME}/$1 \
+    -profile $1 \
+    -profileProperties org.eclipse.update.install.features=true \
+	-bundlepool ${BUNDLE_POOL} \
+    -shared ${PROFILE_HOME} \
+    -p2.os ${TARGET_OS} \
+    -p2.ws ${TARGET_WS} \
+    -p2.arch ${TARGET_ARCH} \
+    -vmargs -Xmx512m
+}
+
+install ${PROFILE_NAME} ${PACKAGE_NAME}
---
+++ p2remove.sh
@@ -0,0 +1,24 @@
+. ./env.rc
+
+INSTALL_NAME=$1
+REPO_LIST=$2
+IU_LIST=$3
+
+# <profile> <repositories list> <IUs list>
+function add {
+    echo Add more artifacts to Eclipse IDE
+	echo ${IU_LIST} from ${REPO_LIST}
+java -jar ${PLATFORM_RUNTIME_HOME}/plugins/org.eclipse.equinox.launcher_*.jar \
+    -nosplash \
+    -application org.eclipse.equinox.p2.director \
+    -repository ${BASE_REPO},$2 \
+    -uninstallIU $3 \
+    -destination ${INSTALL_HOME}/$1 \
+    -profile $1 \
+    -profileProperties org.eclipse.update.install.features=true \
+	-bundlepool ${BUNDLE_POOL} \
+    -shared ${PROFILE_HOME} \
+    -vmargs -Xmx512m
+}
+
+add ${INSTALL_NAME} ${REPO_LIST} ${IU_LIST}
